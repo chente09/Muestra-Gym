2c11d0bca051d1e72e21b73a6f04c972e38df80a
codemasterg19 <pablo_jmz1@hotmail.com>
2024-01-18 23:40:02 -0500

    bbd corregido

--- registro.php
+++ registro.php
@@ -1,10 +1,10 @@
 <?php
     include 'conexion.php';
 
-    $nombres=$_POST['nombres'];
-    $correo=$_POST['correo'];
-    $telefono=$_POST['telefono'];
-    $tipoPlan=$_POST['tipo_plan'];
+    $nombres = $_POST['nombres'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $tipoPlan = $_POST['tipo_plan'];
 
     $consulta = "INSERT into registro(nombres, correo, telefono, tipo_plan) values ('$nombres', '$correo', '$telefono', '$tipoPlan')";
 
