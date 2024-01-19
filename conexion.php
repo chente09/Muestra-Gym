f927b731e0ff21d94472753f9a14f270382b124a
codemasterg19 <pablo_jmz1@hotmail.com>
2024-01-18 23:31:52 -0500

    bdd

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+    $db_host='localhost';
+    $db_usuario='root';
+    $db_password='856fe5bf99J0';
+    $db_database='gimnasio';
+
+    $conexion=mysqli_connect($db_host, $db_usuario, $db_password, $db_database);
+
+
+?>
---
+++ registro.php
@@ -0,0 +1,20 @@
+<?php
+    include 'conexion.php';
+
+    $nombres=$_POST['nombres'];
+    $correo=$_POST['correo'];
+    $telefono=$_POST['telefono'];
+    $tipoPlan=$_POST['tipo_plan'];
+
+    $consulta = "INSERT into registro(nombres, correo, telefono, tipo_plan) values ('$nombres', '$correo', '$telefono', '$tipoPlan')";
+
+    $resultado=mysqli_query($conexion, $consulta);
+
+    if($resultado){
+        echo "Registro exitoso";
+    } else {
+        echo "Error de registro";
+    }
+
+
+?>
