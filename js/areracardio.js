a31638e3b1565107aa8e9504b7390fd635feb088
codemasterg19 <pablo_jmz1@hotmail.com>
2024-01-19 19:04:33 -0500

    funciones

--- js/areracardio.js
+++ js/areracardio.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var joinButton = document.getElementById("joinButton");
-    var modal = document.getElementById("modal");
-    var closeModalButton = document.getElementById("closeModalButton");
-    var submitFormButton = document.getElementById("submitFormButton");
+document.addEventListener("DOMContentLoaded", () => {
+    const joinButton = document.getElementById("joinButton");
+    const modal = document.getElementById("modal");
+    const closeModalButton = document.getElementById("closeModalButton");
+    const submitFormButton = document.getElementById("submitFormButton");
 
-    joinButton.addEventListener("click", function () {
+    joinButton.addEventListener("click", () => {
         modal.style.display = "block";
         modal.classList.add("fadeIn");
     });
 
-    closeModalButton.addEventListener("click", function () {
+    closeModalButton.addEventListener("click", () => {
         modal.style.display = "none";
     });
 
-    submitFormButton.addEventListener("click", function () {
-  
+    submitFormButton.addEventListener("click", () => {
         alert("¡Gracias por mostrar interés en nuestro área de cardio! Te contactaremos pronto.");
         modal.style.display = "none";
     });
